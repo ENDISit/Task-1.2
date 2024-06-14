00c1b71da193e66a48d1f657ee29b9c748f4a1ad
ENDISit <mr.ilia08@mail.ru>
2024-06-14 22:37:09 +0300

    Добавьте файлы проекта.

---
+++ Task 1.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите сумму вклада: ");
+        decimal deposit = Convert.ToDecimal(Console.ReadLine());
+
+        Console.Write("Введите количество месяцев: ");
+        int months = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < months; i++)
+        {
+            deposit += deposit * 0.07m;
+        }
+
+        Console.WriteLine($"Конечная сумма вклада: {deposit}");
+
+        Console.WriteLine("Нажмите любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+}
